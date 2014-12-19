595426bdd59f3117eb419ddc84ed6ecb57961d1b
Michael Hoeller <hoellermichael@gmx.at>
2014-12-19 20:40:49 +0100

    fixed global search

--- js/controllers.js
+++ js/controllers.js
@@ -22,7 +22,6 @@ iidControllers.controller('SearchGlobalController', function($scope) {
 
 
     $scope.goToUni = function(uni) {
-        console.info('goToUni ' + uni.name);
         $scope.selectedUni = uni;
         $scope.selectedLva = null;
         $scope.selectedSemester = null;
@@ -36,8 +35,25 @@ iidControllers.controller('SearchGlobalController', function($scope) {
         $scope.selectedBeispiel = null;
         $scope.selectedTestfile = null;
     };
+    $scope.goToLva2 = function(uni, lva) {
+        $scope.selectedUni = uni;
+        $scope.selectedLva = lva;
+        $scope.selectedSemester = null;
+        $scope.selectedBeispiel = null;
+        $scope.selectedTestfile = null;
+    };
     $scope.goToSemester = function(semester) {
         console.info('goToSemester ' + semester.name);
+        $scope.selectedSemester = semester;
+        $scope.selectedBeispiel = null;
+        $scope.selectedTestfile = null;
+    };
+    $scope.goToSemester2 = function(uni, lva, semester) {
+        alert(uni.name);
+        alert(lva.name);
+        alert(semester.name);
+        $scope.selectedUni = uni;
+        $scope.selectedLva = lva;
         $scope.selectedSemester = semester;
         $scope.selectedBeispiel = null;
         $scope.selectedTestfile = null;
@@ -47,11 +63,31 @@ iidControllers.controller('SearchGlobalController', function($scope) {
         $scope.selectedBeispiel = beispiel;
         $scope.selectedTestfile = null;
     };
+    $scope.goToBeispiel2 = function(uni, lva, semester, beispiel) {
+//        alert(uni.name);
+//        alert(lva.name);
+//        alert(semester.name);
+//        alert(beispiel.name);
+        console.info('goToBeispiel2 ' + testfile.filename);
+        $scope.selectedUni = uni;
+        $scope.selectedLva = lva;
+        $scope.selectedSemester = semester;
+        $scope.selectedBeispiel = beispiel;
+        $scope.selectedTestfile = null;
+    };
     $scope.goToTestfile = function(testfile) {
         console.info('goToTestfile ' + testfile.filename);
         $scope.selectedTestfile = testfile;
     };
-
+    $scope.goToTestfile2 = function(uni, lva, semester, beispiel, contributor, testfile) {
+        console.info('goToTestfile ' + testfile.filename);
+        $scope.selectedUni = uni;
+        $scope.selectedLva = lva;
+        $scope.selectedSemester = semester;
+        $scope.selectedBeispiel = beispiel;
+        $scope.selectedContributor = contributor;
+        $scope.selectedTestfile = testfile;
+    };
 
     $scope.setSelectedLva = function(lva) {
       $scope.selectedLva = lva; 
